3d48e426ec303379607ea7988840dccd8406aea5
Santiago Manganaro Bello <smanganaro@itba.edu.ar>
2019-09-12 13:28:18 -0300

    asdasd

--- src/main/java/ar/edu/itba/model/handlers/Profiler.java
+++ src/main/java/ar/edu/itba/model/handlers/Profiler.java
@@ -29,6 +29,10 @@ public class Profiler {
         return persons;
     }
 
+    public static List<Person> getPersons() {
+        return persons;
+    }
+
     public static void setProfiles(final Map<Profile, Integer> l) {
         profiles = l;
     }
--- src/main/java/ar/edu/itba/processing/Sketch.java
+++ src/main/java/ar/edu/itba/processing/Sketch.java
@@ -1,6 +1,12 @@
 package ar.edu.itba.processing;
 
+import ar.edu.itba.model.Person;
+import ar.edu.itba.model.handlers.Profiler;
 import processing.core.PApplet;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Sketch extends PApplet {
 
@@ -22,10 +28,14 @@ public class Sketch extends PApplet {
         fill(color(97, 192, 255));
         arc(65,32,20, 20, PI, 2*PI);
         circle(65, 15, 20);*/
+        List<Person> personList = Profiler.getPersons();
+        Map<String, Long> parties = personList.stream().collect(Collectors.groupingBy(Person::getPoliticalOrientation, Collectors.counting()));
 
-        for (int i = 0; i < 100; i = i+25) {
-            arc(15+i,32,20, 20, PI, 2*PI);
-            circle(15+i, 15, 20);
+        for (final Map.Entry<String, Long> e : parties.entrySet()) {
+            for (int i = 0; i < e.getValue()/10; i = i+25) {
+                arc(15+i,32,20, 20, PI, 2*PI);
+                circle(15+i, 15, 20);
+            }
         }
 
 
