97353e67ff063396d2d6e45e8e63c7e103a7bfdd
Santiago Manganaro Bello <smanganaro@itba.edu.ar>
2019-11-03 23:11:00 -0300

    Starting UI for EconomicAction

---
+++ src/main/java/ar/edu/itba/ui/election/EconomicActionScoreChart.java
@@ -0,0 +1,14 @@
+package ar.edu.itba.ui.election;
+
+import org.jfree.data.category.CategoryDataset;
+
+public class EconomicActionScoreChart extends EconomicActionBaseChart {
+
+    public EconomicActionScoreChart(String title) {
+        super(title);
+    }
+
+    protected CategoryDataset createDataset() throws Exception {
+        return createDataset();
+    }
+}
