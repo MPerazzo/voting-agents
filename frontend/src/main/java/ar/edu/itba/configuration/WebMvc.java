bac73fb92fd706c3e0c12bea4dc8a9813a1e0fef
Matias Perazzo <mperazzo@prismamp.com>
2019-12-01 12:00:50 -0300

    Try to disable cache

--- frontend/src/main/java/ar/edu/itba/configuration/WebMvc.java
+++ frontend/src/main/java/ar/edu/itba/configuration/WebMvc.java
@@ -1,12 +1,22 @@
 package ar.edu.itba.configuration;
 
 import org.springframework.core.Ordered;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 public class WebMvc implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("resources/image/")
+                .setCachePeriod(0)
+                .setCacheControl(CacheControl.noCache());
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
